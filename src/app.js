57c959881f996d123f5be7fdcd88c1d6db3e8fb2
Joanna Dybowska <joanna.m.dybowska@gmail.com>
2023-04-22 15:24:54 +0200

    Setting up GET /hello-world

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
-
 const app = express();
+const port = 3000;
+
+app.get("/hello-world", (req, res) => {
+  res.send({ msg: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 module.exports = app;
