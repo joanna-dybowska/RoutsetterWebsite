3ff6d969f37831045fda4e5e0599a5798420a661
Joanna Dybowska <joanna.m.dybowska@gmail.com>
2023-04-25 23:50:49 +0200

    Uploading and validating wall image (multer)

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
+const multer = require("multer");
 const app = express();
 const port = 3000;
+
+const upload = multer({
+  dest: "wall-images",
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload an image"));
+    }
+    cb(undefined, true);
+  },
+});
 
 app.get("/hello-world", (req, res) => {
   res.send({ msg: "Hello World!" });
 });
 
-app.post("/wall-image", (req, res) => {
-  res.status(500).send({ error: "Not implemented" });
-});
-
+app.post(
+  "/wall-image",
+  upload.single("wall-image"),
+  async (req, res) => {
+    res.send({ msg: "File uploaded to 'wall-images'" });
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = app;
