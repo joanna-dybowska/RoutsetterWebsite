bd5cc48dc7bdf1c845054707f7f0e52de5c29138
Marcin Kolenda <marcinkolenda419@gmail.com>
2023-07-08 16:29:37 +0200

    Install bootstrap 5.3.0

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,27 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <p>See bootstrap example called "Centered hero" below:</p>
+      <div class="px-4 py-5 my-5 text-center">
+          <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+          <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+          <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+              <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+            </div>
+          </div>
+          <div class="card" style={{width: '18rem'}}>
+            <img src="..." class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
